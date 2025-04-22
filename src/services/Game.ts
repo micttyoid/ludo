32c9fd37687d236f1b5d13cd1ac9df834623178f
Luke Yoo <w.lukeyoo@gmail.com>
2025-04-21 20:22:22 -0700

    Adjust service Game

--- src/services/Game.ts
+++ src/services/Game.ts
@@ -2,28 +2,40 @@ import { Player } from '#models/Player'
 import { Pawn } from '#models/Pawn'
 
 export class Game {
-    private players: Player[] = []
-    private pieces: Pawn[] = []
+    private _players: Player[] = []
+    private _pawns: Pawn[] = []
+    private _max_players: number
 
-    add_player(id: string, name: string): Player {
+    constructor(max_players: number = 4) {
+        // Typical Ludo is 4 player max
+        this._max_players = max_players
+    }
+
+    add_player(id: string, name: string): Player | null {
+        if (this._players.length >= this._max_players) {
+            throw new Error(
+                `Exceeded the maximum number of players (${this._max_players}). Cannot add player ${name}.`
+            )
+        }
+
         const player = new Player(id, name)
-        this.players.push(player)
+        this._players.push(player)
         return player
     }
 
     create_piece(id: string, x: number, y: number, type: string): Pawn {
         const piece = new Pawn(id, { x, y }, type)
-        this.pieces.push(piece)
+        this._pawns.push(piece)
         return piece
     }
 
     assign_piece(pieceId: string, playerId: string): boolean {
-        const piece = this.pieces.find((p) => p.id === pieceId)
-        const player = this.players.find((p) => p.id === playerId)
+        const piece = this._pawns.find((p) => p.id === pieceId)
+        const player = this._players.find((p) => p.id === playerId)
 
         if (piece && player) {
             player.add_piece(piece)
-            this.pieces = this.pieces.filter((p) => p.id !== pieceId)
+            this._pawns = this._pawns.filter((p) => p.id !== pieceId)
             return true
         }
         return false
