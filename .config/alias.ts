f302fc2afb4003efa47d908d2c341cb8e2c1ccfa
Luke Yoo <w.lukeyoo@gmail.com>
2025-04-22 20:33:35 -0700

    Update aliases to have src/types

--- .config/alias.ts
+++ .config/alias.ts
@@ -4,15 +4,17 @@ import { resolve } from 'path'
 export const alias = {
     ts_config_paths: {
         // each pair, key and value, is wild-carded automatically
-        services: [resolve(__dirname, '../src/services')],
-        models: [resolve(__dirname, '../src/models')],
-        utils: [resolve(__dirname, '../src/utils')],
         interfaces: [resolve(__dirname, '../src/interfaces')],
+        models: [resolve(__dirname, '../src/models')],
+        services: [resolve(__dirname, '../src/services')],
+        types: [resolve(__dirname, '../src/types')],
+        utils: [resolve(__dirname, '../src/utils')],
     },
     package_json_imports: {
-        services: resolve(__dirname, '../dist/services'),
+        interfaces: resolve(__dirname, '../dist/interfaces'),
         models: resolve(__dirname, '../dist/models'),
+        services: resolve(__dirname, '../dist/services'),
+        types: [resolve(__dirname, '../dist/types')],
         utils: resolve(__dirname, '../dist/utils'),
-        interfaces: resolve(__dirname, '../src/interfaces'),
     },
 } as const
